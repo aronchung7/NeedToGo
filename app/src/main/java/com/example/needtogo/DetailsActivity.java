b4d40ded1bf15dcd6982844a3a570e280b9e452d
Aron Chung <aronchung77@gmail.com>
2020-11-28 03:00:56 -0500

    Detail Page Design Enhancement

--- app/src/main/java/com/example/needtogo/DetailsActivity.java
+++ app/src/main/java/com/example/needtogo/DetailsActivity.java
@@ -15,6 +15,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+
+import android.view.View;
 import android.widget.TextView;
 import android.os.Bundle;
 
@@ -32,10 +34,9 @@ public class DetailsActivity extends AppCompatActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        System.out.println(db.document(documentPath).getId());
+        final String name = db.document(documentPath).getId();
 
         final TextView details = (TextView) findViewById(R.id.detailsText);
-
 
         db.document(documentPath).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -43,16 +44,34 @@ public class DetailsActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        details.setText(document.getData().toString());
+                        String data = document.getData().toString();
+                        String address = document.getString("address");
+                        String city = document.getString("city");
+                        String country = document.getString("country");
+                        String description = document.getString("description");
+                        boolean availl = document.getBoolean("availability");
+                        String avail = "";
+                        if (availl) {
+                            avail = "Available";
+                        }
+                        else {
+                            avail = "Not Available";
+                        }
+                        double rating = document.getDouble("rating");
+                        details.setText("Name: " + name + "\nAddress: " + address + "\nCity: " + city + "\nCountry: " + country + "\nDescription: " + description + "\nAvailability: " + avail + "\nRating: " + rating);
                         setTitle(document.getId());
                     } else {
                         Log.d(TAG, "No such document");
                     }
                 } else {
-                    Log.d(TAG, "get failed with ", task.getException());
+                    Log.d(TAG, "Get failed with ", task.getException());
                 }
             }
         });
 
+    }
+
+    public void home(View view) {
+        startActivity(new Intent(getApplicationContext(), MainPage.class));
     }
 }
