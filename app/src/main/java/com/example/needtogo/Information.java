336da7701812b29809fd6451868d862a6cb323f0
Aron Chung <aronchung77@gmail.com>
2020-11-28 01:24:09 -0500

    Information Page Design Enhancement and Splash Screen Implemented

--- app/src/main/java/com/example/needtogo/Information.java
+++ app/src/main/java/com/example/needtogo/Information.java
@@ -1,7 +1,10 @@
 package com.example.needtogo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Information extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+    }
+
+    public void home(View view) {
+        startActivity(new Intent(getApplicationContext(), MainPage.class));
     }
 }
